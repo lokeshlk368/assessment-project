4f04a32cc333e23a0f36101c60a5964e0fa57d21
lokeshlk368 <57345886+lokeshlk368@users.noreply.github.com>
2019-12-04 13:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+function weather(){
+    console.log("Hello");
+    var city = document.getElementById("value").value;
+    //var city = $("#value").val();
+    console.log(city);
+    var xmlhttp= new XMLHttpRequest();
+
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+city + "&APPID=eeea50be775f90612cf6d5907e36902d";
+    xmlhttp.open("GET", url, true);
+
+    xmlhttp.send();
+    xmlhttp.onreadystatechange=function()
+    {
+        if(this.readyState === 4 && this.status === 200)
+        {
+            var result=this.responseText;
+            console.log(result);
+
+          var x = JSON.parse(result);
+         console.log(x.main.temp);
+          document.getElementById("temp").innerHTML = "<h3><u>Temperature:</u><h3> " + x.main.temp +" F";
+          console.log(x.weather.description);
+          document.getElementById("speed").innerHTML = "<h3><u>Wind Speed: </u><h3>" + x.wind.speed+" m/s";
+          console.log(x.main.humidity);
+          document.getElementById("humidity").innerHTML = "<h3><u>Humidity:</u><h3> " + x.main.humidity;
+        }
+    };
+
+
+}
+
+
+
+
 $(document).ready(function() {
 
 $("#search").on("click", function() {
@@ -58,33 +92,3 @@ select();
 
 
 
-function weather(){
-    console.log("Hello");
-    var city = document.getElementById("value").value;
-    //var city = $("#value").val();
-    console.log(city);
-    var xmlhttp= new XMLHttpRequest();
-
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+city + "&APPID=eeea50be775f90612cf6d5907e36902d";
-    xmlhttp.open("GET", url, true);
-
-    xmlhttp.send();
-    xmlhttp.onreadystatechange=function()
-    {
-        if(this.readyState === 4 && this.status === 200)
-        {
-            var result=this.responseText;
-            console.log(result);
-
-          var x = JSON.parse(result);
-         console.log(x.main.temp);
-          document.getElementById("temp").innerHTML = "<h3><u>Temperature:</u><h3> " + x.main.temp +" F";
-          console.log(x.weather.description);
-          document.getElementById("speed").innerHTML = "<h3><u>Wind Speed: </u><h3>" + x.wind.speed+" m/s";
-          console.log(x.main.humidity);
-          document.getElementById("humidity").innerHTML = "<h3><u>Humidity:</u><h3> " + x.main.humidity;
-        }
-    };
-
-
-}
